2fc76c4c0155d66ccdd3e563990f5ed57a5f0f10
Pierre Chigot <pierre.chigot@gmail.com>
2019-09-02 00:49:11 +0200

    ajout d'un recyclerview

--- app/src/main/java/com/pierre/mareu/ui/meeting/meeting_list/ListMeetingActivity.java
+++ app/src/main/java/com/pierre/mareu/ui/meeting/meeting_list/ListMeetingActivity.java
@@ -1,17 +1,19 @@
 package com.pierre.mareu.ui.meeting.meeting_list;
 
 import android.os.Bundle;
+
 import android.view.Menu;
-
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
+import androidx.recyclerview.widget.RecyclerView;
 import com.pierre.mareu.R;
+import com.pierre.mareu.ui.meeting.meeting_list.dummy.DummyContent;
 
-public class ListMeetingActivity extends AppCompatActivity {
 
 
+public class ListMeetingActivity extends AppCompatActivity implements MeetingFragment.OnListFragmentInteractionListener {
+
+    private RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +21,22 @@ public class ListMeetingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_meeting_list);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        MeetingFragment.newInstance(1);
+
 
     }
+
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu,menu);
 
         return true;
+    }
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+
     }
 }
